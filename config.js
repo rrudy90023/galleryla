8b5ac74c5ab5f30475d1e5c05e32a9fd74751ff9
Rudy Perez <rrudy90023@gmail.com>
2016-04-06 00:06:07 -0700

    new scheme and vr add 2

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 var config = {};
 
-config.mongoUri = 'mongodb://rrudy90023:y&$XG3!K@ds057234.mlab.com:57234/galleryla';
+config.mongoUri = 'mongodb://rrudy90023:y&$XG3!K@ds015690.mlab.com:15690/galleryla';
 //config.mongoUri = 'mongodb://localhost:27017/galleryla';
 config.cookieMaxAge = 30 * 24 * 3600 * 1000;
 
